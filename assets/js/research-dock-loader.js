8689bab5b6b0210b2fd38d04675d7bf50d8062e6
Gyu1718 <140826799+Gyu1718@users.noreply.github.com>
2026-08-01 19:32:53 +0900

    Load normalized Acts layout and full chapter navigation

--- assets/js/research-dock-loader.js
+++ assets/js/research-dock-loader.js
@@ -5,7 +5,7 @@
   var script = document.currentScript;
   var siteRoot = script && script.src ? new URL("../../", script.src).href : new URL("./", location.href).href;
   function addStyle(path, marker) { if (document.querySelector('link[' + marker + ']')) return; var link=document.createElement("link");link.rel="stylesheet";link.href=new URL(path,siteRoot).href;link.setAttribute(marker,"");document.head.appendChild(link); }
-  function addScript(path, marker) { if (document.querySelector('script[' + marker + ']')) return; var node=document.createElement("script");node.src=new URL(path,siteRoot).href;node.setAttribute(marker,"");document.head.appendChild(node); }
+  function addScript(path, marker) { if (document.querySelector('script[' + marker + ']')) return; var node=document.createElement("script");node.src=new URL(path,siteRoot).href;node.async=false;node.setAttribute(marker,"");document.head.appendChild(node); }
   addStyle("assets/theme.css?v=20260724.2", "data-rd-theme-css");
   addStyle("assets/app.css?v=20260724.2", "data-rd-app-css");
   addStyle("assets/css/study-navigation-policy.css?v=20260725.1", "data-study-navigation-policy-css");
@@ -14,7 +14,11 @@
     addScript("assets/js/book-shelf-enhancements.js?v=20260801.2", "data-book-shelf-enhancements-js");
   }
   if (/\/nt\/acts\//i.test(location.pathname) || (document.body && document.body.getAttribute("data-book")==="acts")) {
-    addScript("assets/js/acts-chapter-links.js?v=20260801.4", "data-acts-chapter-links-js");
+    if (document.body && document.body.getAttribute("data-kind")==="study") {
+      addStyle("assets/css/acts-study-normalize.css?v=20260801.5", "data-acts-study-normalize-css");
+      addScript("assets/js/acts-layout-normalizer.js?v=20260801.5", "data-acts-layout-normalizer-js");
+    }
+    addScript("assets/js/acts-chapter-links.js?v=20260801.5", "data-acts-chapter-links-js");
   }
   if (/\/nt\/romans\//i.test(location.pathname)) addScript("assets/js/romans-supplement.js?v=20260726.1", "data-romans-supplement-js");
   if (/\/ot\/nehemiah\//i.test(location.pathname) || document.querySelector("[data-encyclopedia-scan],.encyclopedia-scan,.dict-scan,[data-entity]")) addScript("assets/js/encyclopedia-loader.js?v=20260730.1", "data-encyclopedia-loader-js");
